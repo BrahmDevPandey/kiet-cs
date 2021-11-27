481fcddce2fa6bb4e9eef337c07283083a56384c
Brahm Dev Pandey <brahmdevpandey762@gmail.com>
2021-11-27 22:14:20 +0530

    patterns completed

---
+++ c_self/right_triangle.c
@@ -0,0 +1,20 @@
+// program to print a right aligned right angle triangle pattern of *s
+#include <stdio.h>
+
+int main()
+{
+    int rows = 0, stars = 0, spaces = 0, i;
+    printf("Enter the number of rows to be printed: ");
+    scanf("%d", &rows);
+
+    // printing the pattern using spaces and stars
+    for (i = 1; i <= rows; i++)
+    {
+        for (spaces = rows - i; spaces > 0; spaces--)
+            printf("  ");
+        for (stars = 1; stars <= i; stars++)
+            printf("* ");
+        printf("\n");
+    }
+    return 0;
+}
